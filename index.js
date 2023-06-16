2f1a178f7add4868dafd2e60885332e91eaf14c7
adamKellyObare <aobarekelly@gmail.com>
2023-06-16 12:30:53 +0300

    Corrected Assignment

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const currentUser = 'Adam Kelly';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}!`;
+const shortGreeting = "Welcome, $Adam.slice([4][3][2][1])!";
 
